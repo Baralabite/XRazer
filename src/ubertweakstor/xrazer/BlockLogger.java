64ca049730e53414abeecc638e00ec51d0b5ac48
boar401s2 <johnsBogAcc@gmail.com>
2013-01-26 15:58:06 +1000

    Name: XRazer Auther: John Board Copyright: (c) 2012 - 2013 John Board Date: 18/12/2012 20:59 Description: XRay Detector for Tekkit Server v1.2.5
    ====================================================================
    Change Log:
    
    v1.2.6:
     - Moved to Git

---
+++ src/ubertweakstor/xrazer/BlockLogger.java
@@ -0,0 +1,93 @@
+package ubertweakstor.xrazer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+
+public class BlockLogger {
+	
+	XRazer parent;
+	HashMap<Player, Score> scores = new HashMap<Player, Score>();
+	
+	/*
+	 * Name: BlockLogger 
+	 * Description: Constructor
+	 * Returns: None
+	 * Parameters: XRazer
+	 * Requirements: None
+	 */
+	BlockLogger(XRazer instance){
+		parent = instance;
+	}
+	
+	/*
+	 * Name: incrementBlockScore
+	 * Description: Increments the numbers of blocks mined in the subsiquent catagories
+	 * Returns: None
+	 * Parameters: int id: Block ID, int data: Block data, Player player: Which player's 
+	 *             profile to update
+	 * Requirements: None
+	 */
+	public void incrementBlockScore(int id, int data, Player player){
+		Score score;
+		if (scores.containsKey(player)){
+			score = scores.get(player);
+		}
+		else{
+			score = new Score(this);			
+		}
+		score.incrementBlockIDScore(id, data);
+		scores.put(player, score);
+	}
+	
+	
+	public Score getPlayerScore(Player player){
+		return scores.get(player);
+	}
+	
+	/*
+	 * Name: getPossibleXrayers 
+	 * Description: Returns List of all the players suspected of xraying since last check.
+	 * Returns: List<Player>
+	 * Parameters: None
+	 * Requirements: None
+	 */
+	@SuppressWarnings("rawtypes")
+	public List<Player> getPossibleXrayers(){
+		List<Player> possibleXrayers = new ArrayList<Player>();
+	    Iterator it = scores.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pairs = (Map.Entry)it.next();
+	        Score score = (Score) pairs.getValue();	        
+	        if (score.getTotalScore() >= parent.getConfig().getInt("XRayThreshold") && !(parent.isPlayerWhitelisted(((Player) pairs.getKey()).getName()))){
+	        	possibleXrayers.add((Player) pairs.getKey());
+	        }	    
+	    }
+	    return possibleXrayers;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public void clearScores(){
+		Iterator it = scores.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pairs = (Map.Entry)it.next();
+	        scores.remove((Player) pairs.getKey());
+	    }
+	}
+	
+	/*
+	 * Name: getBlockID
+	 * Description: Get's the blockid, from the id, and data
+	 * Returns: String, similar to: "35:0", which would be white wool
+	 * Parameters: int id: The block's id, int data: The data of the block
+	 * Requirements: None
+	 */
+	public String getBlockID(int id, int data){
+		return String.valueOf(id)+":"+String.valueOf(data);
+	}	
+	
+}
---
+++ src/ubertweakstor/xrazer/Score.java
@@ -0,0 +1,100 @@
+package ubertweakstor.xrazer;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Score{
+	
+	public HashMap<String, Integer> score = new HashMap<String, Integer>();
+	BlockLogger parent;
+	
+	/*
+	 * Name: Score 
+	 * Description: Constructor 
+	 * Returns: None
+	 * Parameters: BlockLogger
+	 * Requirements: None
+	 */
+	Score(BlockLogger instance){
+		parent = instance;
+	}
+	
+	//=====[Change Score]=====//
+	
+	/*
+	 * Name: incrementBlockIDScore
+	 * Description: Increment a certain BlockID's score
+	 * Returns: None
+	 * Parameters: int id: Block's ID, int data: Block's Data
+	 * Requirements: None
+	 */
+	public void incrementBlockIDScore(int id, int data){
+		String blockID = parent.getBlockID(id, data);
+		if (score.containsKey(blockID)==false){
+			score.put(blockID, 1);
+		} else {
+			score.put(blockID, score.get(blockID)+1);
+		}
+	}	
+	
+	/*
+	 * Name: setBlockIDScore
+	 * Description: Set a certain BlockID's score
+	 * Returns: None
+	 * Parameters: int id: Block's ID, int data: Block's Data, int score: Score to set
+	 * Requirements: None
+	 */
+	public void setBlockIDScore(int id, int data, int value){
+		String blockid = parent.getBlockID(id, data);
+		score.put(blockid, value);
+	}
+	
+	//=====[Gets]=====//
+	
+	/*
+	 * Name: getBlockIDScore
+	 * Description: Gets a certain BlockID's score
+	 * Returns: Integer
+	 * Parameters: int id: Block's ID, int data: Block's Data
+	 * Requirements: None
+	 */
+	public Integer getBlockIDScore(int id, int data){
+		return score.get(parent.getBlockID(id, data));
+	}
+	
+	/*
+	 * Name: getTotalScore
+	 * Description: Gets the score of everything
+	 * Returns: Integer
+	 * Parameters: None
+	 * Requirements: None
+	 */
+	@SuppressWarnings("rawtypes")
+	public Integer getTotalScore() {
+		int total = 0;
+	    Iterator it = score.entrySet().iterator();
+	    while (it.hasNext()) {	    	
+	        Map.Entry pairs = (Map.Entry)it.next();
+	        total = total + (Integer) pairs.getValue();
+	    }
+	    return total;
+	}
+	
+	public HashMap<String, Integer> getScore(){
+		return score;
+	}
+	
+	//=====[Misc]=====//
+	
+	/*
+	 * Name: clearData
+	 * Description: Clears the hashmap
+	 * Returns: None
+	 * Parameters: None
+	 * Requirements: None
+	 */
+	public void clearData(){
+		score = new HashMap<String, Integer>();
+	}
+}
